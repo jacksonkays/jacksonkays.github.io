630267e63f065749024cdda7bc340eb813ac344e
jacksonkays <55301666+jacksonkays@users.noreply.github.com>
2020-08-18 12:31:58 -0500

    Reconfigured Contact Form with JavaScript

--- main.js
+++ main.js
@@ -21,6 +21,36 @@ $(document).ready(function () {
         }, 1000);
     });
 
+    $(document).ready(function () {
+        $('.submit').click(function (event){
+            event.preventDefault()
+            console.log('Clicked Button')
+
+            var name = $('.name').val()
+            var email = $('.email').val()
+            var message = $('.message').val()
+            var statusElm = $('.status')
+            statusElm.empty()
+
+            if(email.length > 5 && email.includes('@') && email.includes('.')) {
+                statusElm.append('<div>Email is valid.</div>')
+            } else {
+                statusElm.append('<div>Email is invalid.</div>')
+            }
+
+            if(name.length > 2) {
+                statusElm.append('<div>Name is not valid.</div>')
+            } else {
+                statusElm.append('<div>Name is not valid.</div>')
+            }
+            if(message.length > 10) {
+                statusElm.append('<div>Message length is valid.</div>')
+            } else {
+                statusElm.append('<div>Message length is not valid.</div>')
+            }
+        })
+    })
+
     AOS.init({
         easing: 'ease',
         duration: 1800,
